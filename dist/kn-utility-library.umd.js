afddf366221d09492500909592c6c6f4b883f113
maruthi_edien <maruthi@edien.com>
2024-12-19 17:34:15 +0530

    test

--- dist/kn-utility-library.umd.js
+++ dist/kn-utility-library.umd.js
@@ -1,1 +1,16 @@
-(function(e,t){typeof exports=="object"&&typeof module<"u"?t(exports):typeof define=="function"&&define.amd?define(["exports"],t):(e=typeof globalThis<"u"?globalThis:e||self,t(e.Cb={}))})(this,function(e){"use strict";const t=(n,i="div")=>{if(!/<\/?[a-z][\s\S]*>/i.test(n))return n;const o=document.createElement(i);return o.textContent=n,o.outerHTML};e.wrapTextInTag=t,Object.defineProperty(e,Symbol.toStringTag,{value:"Module"})});
+(function (e, t) {
+  typeof exports == "object" && typeof module < "u"
+    ? t(exports)
+    : typeof define == "function" && define.amd
+    ? define(["exports"], t)
+    : ((e = typeof globalThis < "u" ? globalThis : e || self), t((e.Cb = {})));
+})(this, function (e) {
+  "use strict";
+  const t = (n, i = "div") => {
+    if (!/<\/?[a-z][\s\S]*>/i.test(n)) return n;
+    const o = document.createElement(i);
+    return (o.textContent = n), o.outerHTML;
+  };
+  (e.wrapTextInTag = t),
+    Object.defineProperty(e, Symbol.toStringTag, { value: "Module" });
+});
