60cacb31e52fab7777607f64e4148066e4f03844
maruthi_edien <maruthi@edien.com>
2024-12-19 22:45:54 +0530

    update

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,17 @@ export const wrapTextInTag = (
   text: string,
   tagName: string = "div"
 ): string => {
-  const hsTgs = /<\/?[a-z][\s\S]*>/i.test(text); // Check if the text already has HTML tags
-  if (!hsTgs) return text; // Return text as-is if no tags
-  const el = document.createElement(tagName); // Create element with specified tag name
-  el.textContent = text; // Set the text content
-  return el.outerHTML; // Return the HTML
+  const hsTgs = /<\/?[a-z][\s\S]*>/i.test(text);
+  if (!hsTgs) return text;
+  const el = document.createElement(tagName);
+  el.textContent = text;
+  return el.outerHTML;
+};
+
+export const generateId = () => {
+  return "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+    .split("")
+    .sort(() => 0.5 - Math.random())
+    .join("")
+    .substring(0, 10);
 };
